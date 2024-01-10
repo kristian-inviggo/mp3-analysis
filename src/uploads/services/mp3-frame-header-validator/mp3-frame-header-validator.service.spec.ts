733e1c8724233f60f616730412479452ffa62960
Kristian Tot <kristian.tot@inviggo.com>
2024-01-10 23:23:30 +0100

    Add test cases for mp3 frame header validator

--- src/uploads/services/mp3-frame-header-validator/mp3-frame-header-validator.service.spec.ts
+++ src/uploads/services/mp3-frame-header-validator/mp3-frame-header-validator.service.spec.ts
@@ -9,10 +9,50 @@ describe('Mp3FrameHeaderValidatorService', () => {
       providers: [Mp3FrameHeaderValidatorService],
     }).compile();
 
-    service = module.get<Mp3FrameHeaderValidatorService>(Mp3FrameHeaderValidatorService);
+    service = module.get<Mp3FrameHeaderValidatorService>(
+      Mp3FrameHeaderValidatorService,
+    );
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('isPotentialHeader', () => {
+    it('should return true for a valid MP3 header', () => {
+      const validHeader = Buffer.from([0xff, 0xe1, 0x00, 0x00]);
+      const result = service.isPotentialHeader(validHeader);
+      expect(result).toBe(true);
+    });
+
+    it('should return false for an invalid MP3 header', () => {
+      const invalidHeader = Buffer.from([0x00, 0xff, 0x00, 0x00]);
+      const result = service.isPotentialHeader(invalidHeader);
+      expect(result).toBe(false);
+    });
+  });
+
+  describe('isValidMP3FrameHeader', () => {
+    describe('should return true for valid header', () => {
+      it('for a valid MP3 frame header', () => {
+        const validHeader = Buffer.from([0xff, 0xe1, 0x00, 0x00]);
+        const result = service.isValidMP3FrameHeader(validHeader);
+        expect(result).toBe(true);
+      });
+    });
+
+    describe('should return false for invalid header', () => {
+      it('for invalid MP3 frame header with incorrect sync bits', () => {
+        const invalidHeader = Buffer.from([0x00, 0xff, 0x00, 0x00]);
+        const result = service.isValidMP3FrameHeader(invalidHeader);
+        expect(result).toBe(false);
+      });
+
+      it('for invalid MP3 frame header with incorrect version and layer', () => {
+        const invalidHeader = Buffer.from([0xff, 0xff, 0x00, 0x00]);
+        const result = service.isValidMP3FrameHeader(invalidHeader);
+        expect(result).toBe(false);
+      });
+    });
   });
 });
