ed24c7ae93e0c8e8a5666906eb859763333c3da8
Kristian Tot <kristian.tot@inviggo.com>
2024-01-11 03:59:14 +0100

    Add global header for swagger

--- src/swagger/swagger-initializer.ts
+++ src/swagger/swagger-initializer.ts
@@ -5,6 +5,11 @@ export function swaggerInitializer(app: INestApplication): OpenAPIObject {
   const config = new DocumentBuilder()
     .setTitle('NestJS Web API for MP3Analysis')
     .setVersion('1.0')
+    .addGlobalParameters({
+      in: 'header',
+      required: false,
+      name: 'x-correlation-id',
+    })
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
