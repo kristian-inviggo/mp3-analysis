9be1e9eeda7d1bfd56e7919fa9d2e9e905b41e6e
Kristian Tot <kristian.tot@inviggo.com>
2024-01-12 11:42:41 +0100

    Add custom js and css urls for swagger

--- src/swagger/swagger-initializer.ts
+++ src/swagger/swagger-initializer.ts
@@ -14,7 +14,14 @@ export function swaggerInitializer(app: INestApplication): OpenAPIObject {
 
   const document = SwaggerModule.createDocument(app, config);
 
-  SwaggerModule.setup('docs', app, document);
+  SwaggerModule.setup('docs', app, document, {
+    customCssUrl:
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.min.css',
+    customJs: [
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-bundle.js',
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.js',
+    ],
+  });
 
   return document;
 }
