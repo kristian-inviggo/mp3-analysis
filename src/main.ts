ea016ca83b8c7806570693f6b7b96620d3a145e3
Kristian Tot <kristian.tot@inviggo.com>
2024-01-12 11:28:56 +0100

    initalize swagger on prod env

--- src/main.ts
+++ src/main.ts
@@ -6,9 +6,7 @@ import { Logger } from 'nestjs-pino';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { abortOnError: false });
 
-  if (process.env.NODE_ENV !== 'production') {
-    swaggerInitializer(app);
-  }
+  swaggerInitializer(app);
 
   app.useLogger(app.get(Logger));
   await app.listen(3000);
